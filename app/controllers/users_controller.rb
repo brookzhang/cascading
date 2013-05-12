6c08a4a0bca8f2e2d616f33ee730390c110a346f
brookzhang <brook_realize@126.com>
2013-05-12 16:58:26 +0800

    clear area & neighborhood while city is nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,11 +30,25 @@ class UsersController < ApplicationController
   
   
    def update_areas
+    city_id =  (params[:city_id].nil? || params[:city_id].empty?) ? 0 : params[:city_id].to_i
+     
       # updates artists and songs based on genre selected
-      city = City.find(params[:city_id])
-      # map to name and id for use in our options_for_select
-      @areas = city.areas.map{|a| [a.name, a.id]}.insert(0, "Select an Area")
-      @neighborhoods = Neighborhood.where(:area_id => city.areas.each{|a| a.id}).map{|s| [s.name, s.id]}.insert(0, "Select a neighborhood")
+      if city_id == 0
+        @areas = []
+        @neighborhoods = []
+        
+      else
+        city = City.find(params[:city_id])
+        # map to name and id for use in our options_for_select
+        @areas = city.areas.map{|a| [a.name, a.id]}
+        @neighborhoods = Neighborhood.where(:area_id => city.areas.each{|a| a.id}).map{|s| [s.name, s.id]}
+        
+      end
+      
+      @areas.insert(0, "Select an Area")
+      @neighborhoods.insert(0, "Select a neighborhood") 
+      
+      
     end
 
     def update_neighborhoods
